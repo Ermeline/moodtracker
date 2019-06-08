02887bc6958c1bc481c160b9f93cf14ed2b4052f
Ermeline Wollknoll <git@flauschiversum.de>
2019-06-08 17:16:32 +0200

    Model.js

---
+++ src/model.js
@@ -0,0 +1,9 @@
+class MoodEntry {
+    constructor(date, emotions, situation, need, behaviour) {
+        this.date = date;
+        this.emotions = emotions;
+        this.situation = situation;
+        this.need = need;
+        this.behaviour = behaviour;
+    }
+}
