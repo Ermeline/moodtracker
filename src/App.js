c7d08614011196c23183125698da514013a8f377
Ermeline Wollknoll <git@flauschiversum.de>
2019-06-08 17:55:38 +0200

    button kann geklickt werden

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import MoodEntry from './model.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let mood_entries = [
+    new MoodEntry("2019-06-07 15:00", ["SADNESS", "FEAR"], "Being home alone", "my girlfriend", "texting her, if she is okay"),
+    new MoodEntry("2019-06-08 16:00", ["HAPPINESS"], "Being with my girlfriend", "Blowjob", "sucking her tits"),
+]
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = mood_entries[0];
+    }
+    change_mood() {
+        this.setState(mood_entries[1]);
+    }
+    render()
+    {
+        return (
+            <div>
+                Am {this.state.date} fühltest du dich <Emotion emotion={this.state.emotions[0]}></Emotion>.
+                <button onClick={() => this.change_mood()}>Gefühl ändern</button>
+            </div>
+        );
+    }
 }
+
+class Emotion extends React.Component {
+    render() {
+        return <div class={this.props.emotion}>{this.props.emotion}</div>
+    }
+
+}
+
 
 export default App;
--- src/model.js
+++ src/model.js
@@ -7,3 +7,4 @@ class MoodEntry {
         this.behaviour = behaviour;
     }
 }
+export default MoodEntry;
